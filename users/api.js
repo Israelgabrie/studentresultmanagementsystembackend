8b28a154f2b41c228bbff232381c9dbca831389e
Israelgabrie <101274418+Israelgabrie@users.noreply.github.com>
2025-07-27 20:13:16 +0100

    latest-commit

--- users/api.js
+++ users/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userRouter = express.Router();
 const userModel = require("../databaseConnection").User;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
